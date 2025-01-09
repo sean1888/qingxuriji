4e64691e7dbaf691c108ce080399dbb61988c757
duyu9 <duyu9@tal.com>
2025-01-09 16:56:17 +0800

    about

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,14 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    version: '1.0.0'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // 获取系统信息
+    const systemInfo = wx.getSystemInfoSync()
+    this.setData({
+      systemInfo
+    })
+    
+    // 检查更新
+    this.checkUpdate()
   },
 
   /**
@@ -62,5 +69,50 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  // 复制邮箱
+  copyEmail() {
+    wx.setClipboardData({
+      data: 'support@qingxuriji.com',
+      success: () => {
+        wx.showToast({
+          title: '邮箱已复制',
+          icon: 'success'
+        })
+      }
+    })
+  },
+
+  // 复制微信
+  copyWechat() {
+    wx.setClipboardData({
+      data: 'qingxuriji',
+      success: () => {
+        wx.showToast({
+          title: '微信号已复制',
+          icon: 'success'
+        })
+      }
+    })
+  },
+
+  // 检查更新
+  checkUpdate() {
+    const updateManager = wx.getUpdateManager()
+    
+    updateManager.onCheckForUpdate(function (res) {
+      if (res.hasUpdate) {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success: function (res) {
+            if (res.confirm) {
+              updateManager.applyUpdate()
+            }
+          }
+        })
+      }
+    })
   }
 })
